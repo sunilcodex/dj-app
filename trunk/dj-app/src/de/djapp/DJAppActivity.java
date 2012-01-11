9dcecef761ebf5c30e4213a9e80ff94ef5b6ecc4
krings.andreas <krings.andreas@919e83dc-b2b1-b480-9c7b-3340a5a4d5e6>
2012-01-11 18:18:17 +0000

    * format

--- trunk/dj-app/src/de/djapp/DJAppActivity.java
+++ trunk/dj-app/src/de/djapp/DJAppActivity.java
@@ -3,11 +3,13 @@ package de.djapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class DJAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class DJAppActivity extends Activity
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.main);
+	}
 }
